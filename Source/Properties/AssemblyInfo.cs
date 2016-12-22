25de772c86be3c6f7d05687c939c798ae352dd28
Phineas Freak <PhineasFreak@users.noreply.github.com>
2016-12-22 14:25:39 +0200

    A range is better

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("NathanKell and ferram4")]
 [assembly: AssemblyProduct("RealHeat")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2015 - 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
